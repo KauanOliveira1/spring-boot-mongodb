1ce342d53d837e1a0e41007cf71859bf9b51709b
KauanOliveira1 <oliveirakauan7@gmail.com>
2024-10-29 22:51:14 -0300

    PUT e DELETE

--- src/main/java/com/project/workshopmongo/services/UserService.java
+++ src/main/java/com/project/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.workshopmongo.domain.User;
+import com.project.workshopmongo.dto.UserDTO;
 import com.project.workshopmongo.repository.UserRepository;
 import com.project.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,28 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		Optional<User> optionalUser = repo.findById(obj.getId());
+		User newObj = optionalUser.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
 }
