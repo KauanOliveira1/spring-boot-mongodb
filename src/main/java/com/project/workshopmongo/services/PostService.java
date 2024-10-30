7c90dfef641b871129540fc48621a48ed5d41740
KauanOliveira1 <oliveirakauan7@gmail.com>
2024-10-30 09:39:46 -0300

    query methods

---
+++ src/main/java/com/project/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.project.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.workshopmongo.domain.Post;
+import com.project.workshopmongo.repository.PostRepository;
+import com.project.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+
+}
